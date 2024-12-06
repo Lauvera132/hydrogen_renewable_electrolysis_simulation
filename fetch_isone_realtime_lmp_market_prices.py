875c7587d96c963455b2cd502fcbb615818d45c7
Laura Rivera <lr28372@my.utexas.edu>
2024-12-06 11:16:25 -0600

    added get_miso_lmp function and modified percentage of missing hours to print for all years

--- fetch_isone_realtime_lmp_market_prices.py
+++ fetch_isone_realtime_lmp_market_prices.py
@@ -40,12 +40,11 @@ def get_isone_rt_lmp(years):
             )
             missing_hours = full_range.difference(df.index)
 
-            if not missing_hours.empty:
-                total_hours = len(full_range)
-                missing_percentage = (len(missing_hours) / total_hours) * 100
-                print(
-                    f"Year {year}: Percentage of missing hourly data: {missing_percentage:.2f}%"
-                )
+            total_hours = len(full_range)
+            missing_percentage = (len(missing_hours) / total_hours) * 100
+            print(
+                f"Year {year} ISO-NE: Percentage of missing hourly data: {missing_percentage:.2f}%"
+            )
         except FileNotFoundError:
             print(f"File not found: {file_path}. Skipping this file.")
 
@@ -116,12 +115,11 @@ def get_nyiso_rt_lmp(years):
             )
             missing_hours = full_range.difference(df.index)
 
-            if not missing_hours.empty:
-                total_hours = len(full_range)
-                missing_percentage = (len(missing_hours) / total_hours) * 100
-                print(
-                    f"Year {year}: Percentage of missing hourly data: {missing_percentage:.2f}%"
-                )
+            total_hours = len(full_range)
+            missing_percentage = (len(missing_hours) / total_hours) * 100
+            print(
+                f"Year {year} NYISO: Percentage of missing hourly data: {missing_percentage:.2f}%"
+            )
         except FileNotFoundError:
             print(f"File not found: {file_path}. Skipping this file.")
 
@@ -132,6 +130,72 @@ def get_nyiso_rt_lmp(years):
         print("No data frames to combine. All files might be missing.")
         return pd.DataFrame()
 
+# MISO
+# Dictionary to rename columns
+column_rename_miso = {
+    "Hour Number": "hour_number",
+    "Arkansas Hub LMP": "arkansas_lmp[$/MWh]",
+    "Illinois Hub LMP": "illinois_lmp[$/MWh]",
+    "Indiana Hub LMP": "indiana_lmp[$/MWh]",
+    "Louisiana Hub LMP": "louisiana_lmp[$/MWh]",
+    "Michigan Hub LMP": "michigan_lmp[$/MWh]",
+    "Minnesota Hub LMP": "minnesota_lmp[$/MWh]",
+    "Mississippi Hub LMP": "mississippi_lmp[$/MWh]",
+    "Texas Hub LMP": "texas_lmp[$/MWh]",
+}
+
+# Function to fetch MISO real-time LMP data
+def get_miso_rt_lmp(years):
+    data_frames = {}
+
+    for year in years:
+        file_path = f"miso_realtime_locational_marginal_prices/miso_lmp_rt_hr_hubs_{year}.csv"
+        try:
+            df = pd.read_csv(
+                file_path,
+                skiprows=3,
+                usecols=range(13),
+                parse_dates=["Local Timestamp Eastern Standard Time (Interval Beginning)"],
+                index_col="Local Timestamp Eastern Standard Time (Interval Beginning)",
+            )
+            df.rename(columns=column_rename_miso, inplace=True)
+            df.index.rename("local_time_int_start", inplace=True)
+            df = df[list(column_rename_miso.values())]
+
+            # Add a new column for average LMP
+            df["miso_average_lmp[$/MWh]"] = (
+                df["arkansas_lmp[$/MWh]"]
+                + df["illinois_lmp[$/MWh]"]
+                + df["indiana_lmp[$/MWh]"]
+                + df["louisiana_lmp[$/MWh]"]
+                + df["michigan_lmp[$/MWh]"]
+                + df["minnesota_lmp[$/MWh]"]
+                + df["mississippi_lmp[$/MWh]"]
+                + df["texas_lmp[$/MWh]"]
+            ) / 8
+
+            data_frames[year] = df
+
+            # Check for missing hours for the year
+            full_range = pd.date_range(
+                start=f"{year}-01-01", end=f"{year}-12-31 23:00:00", freq="h"
+            )
+            missing_hours = full_range.difference(df.index)
+
+            total_hours = len(full_range)
+            missing_percentage = (len(missing_hours) / total_hours) * 100
+            print(
+                f"Year {year} MISO: Percentage of missing hourly data: {missing_percentage:.2f}%"
+            )
+        except FileNotFoundError:
+            print(f"File not found: {file_path}. Skipping this file.")
+
+    if data_frames:
+        combined_df = pd.concat(data_frames.values())
+        return combined_df
+    else:
+        print("No data frames to combine. All files might be missing.")
+        return pd.DataFrame()
 
 # test the function
 if __name__ == "__main__":
@@ -139,15 +203,15 @@ if __name__ == "__main__":
     try:
         isone_data = get_isone_rt_lmp(years)
         nyiso_data = get_nyiso_rt_lmp(years)
-        # miso_data = get_miso_rt_lmp(years)
+        miso_data = get_miso_rt_lmp(years)
         print("ISO-NE Data:")
         print(isone_data.head())
         print(isone_data.tail())
         print("NYISO Data:")
         print(nyiso_data.head())
         print(nyiso_data.tail())
-        # print("MISO Data:")
-        # print(miso_data.head())
-        # print(miso_data.tail())
+        print("MISO Data:")
+        print(miso_data.head())
+        print(miso_data.tail())
     except Exception as e:
         print(f"An error occurred: {e}")
