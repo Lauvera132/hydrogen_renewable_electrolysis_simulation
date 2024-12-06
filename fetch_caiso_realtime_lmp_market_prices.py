cc235accb3dedbe5dd84b6fc12e84d41f11e77c2
Laura Rivera <lr28372@my.utexas.edu>
2024-12-06 13:48:50 -0600

    added caiso_realtime_lmp function and black formatter for readability of other files

--- fetch_caiso_realtime_lmp_market_prices.py
+++ fetch_caiso_realtime_lmp_market_prices.py
@@ -1,43 +1,46 @@
 # fetch wholesale electricity market prices ERCOT RTO/ISO region
 # Quarterly data files are available for 15 minute time intervals
-# https://www.eia.gov/electricity/wholesalemarkets/data.php?rto=ercot
+# https://www.eia.gov/electricity/wholesalemarkets/data.php?rto=caiso
 
 import pandas as pd
 
-# ERCOT
+# CAISO
 # Dictionary to rename columns
 column_rename_dict = {
-    "Local Timestamp Central Time (Interval Ending)": "local_time_int_end",
+    "Local Timestamp Pacific Time (Interval Ending)": "local_time_int_end",
     "UTC Timestamp (Interval Ending)": "utc_time_int_end",
     "Local Date": "local_date",
     "Hour Number": "hour_number",
-    "Bus average LMP": "bus_average_lmp[$/MWh]",
-    "Houston LMP": "houston_lmp[$/MWh]",
-    "Hub average LMP": "ercot_average_lmp[$/MWh]",
-    "North LMP": "north_lmp[$/MWh]",
-    "Panhandle LMP": "panhandle_lmp[$/MWh]",
-    "South LMP": "south_lmp[$/MWh]",
-    "West LMP": "west_lmp[$/MWh]",
+    "NP-15 LMP": "np15_lmp[$/MWh]",
+    "SP-15 LMP": "sp15_lmp[$/MWh]",
+    "ZP-26 LMP": "zp26_lmp[$/MWh]",
 }
 
 
-def get_ercot_rt_lmp(years):
+def get_caiso_rt_lmp(years):
     data_frames = {}
     for year in years:
         yearly_data_frames = []
         for quarter in ["Q1", "Q2", "Q3", "Q4"]:
-            file_path = f"ercot_realtime_locational_marginal_prices/ercot_lmp_rt_15min_hubs_{year}{quarter}.csv"
+            file_path = f"caiso_realtime_locational_marginal_prices/caiso_lmp_rt_15min_zones_{year}{quarter}.csv"
             try:
                 df = pd.read_csv(
                     file_path,
                     skiprows=3,
                     usecols=range(12),
-                    parse_dates=["Local Timestamp Central Time (Interval Beginning)"],
-                    index_col="Local Timestamp Central Time (Interval Beginning)",
+                    parse_dates=["Local Timestamp Pacific Time (Interval Beginning)"],
+                    index_col="Local Timestamp Pacific Time (Interval Beginning)",
                 )
                 df.rename(columns=column_rename_dict, inplace=True)
                 df.index.rename("local_time_int_start", inplace=True)
                 df = df[list(column_rename_dict.values())]
+
+                # Add the new column for average LMP
+                df["caiso_average_lmp[$/MWh]"] = (
+                    df["np15_lmp[$/MWh]"]
+                    + df["sp15_lmp[$/MWh]"]
+                    + df["zp26_lmp[$/MWh]"]
+                ) / 3
 
                 # Select only numeric columns for resampling
                 numeric_columns = df.select_dtypes(include=[float, int]).columns
@@ -79,8 +82,8 @@ def get_ercot_rt_lmp(years):
 if __name__ == "__main__":
     years = [2020, 2021, 2022, 2023]
     try:
-        data = get_ercot_rt_lmp(years)
-        print("ERCOT Data:")
+        data = get_caiso_rt_lmp(years)
+        print("CAISO Data:")
         print(data.head())
         print(data.tail())
     except Exception as e:
