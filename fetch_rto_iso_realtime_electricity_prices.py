086d33bba68a443e879c6e4a2868d0895daf8082
Laura Rivera <lr28372@my.utexas.edu>
2024-12-07 16:14:50 -0600

    capitalized rto_iso regions in fetch file and imported function into calculate_npv

--- fetch_rto_iso_realtime_electricity_prices.py
+++ fetch_rto_iso_realtime_electricity_prices.py
@@ -6,17 +6,17 @@ from fetch_spp_realtime_lmp_market_prices import get_spp_rt_lmp
 from fetch_ercot_realtime_lmp_market_prices import get_ercot_rt_lmp
 
 def get_rto_iso_rt_lmp(rto_iso, years):
-    if rto_iso == 'pjm':
+    if rto_iso == 'PJM':
         df = get_pjm_rt_lmp(years)
-    elif rto_iso == 'nyiso':
+    elif rto_iso == 'NYISO':
         df = get_nyiso_rt_lmp(years)
-    elif rto_iso == 'caiso':
+    elif rto_iso == 'CAISO':
         df = get_caiso_rt_lmp(years)
-    elif rto_iso == 'miso':
+    elif rto_iso == 'MISO':
         df = get_miso_rt_lmp(years)
-    elif rto_iso == 'spp':
+    elif rto_iso == 'SPP':
         df = get_spp_rt_lmp(years)
-    elif rto_iso == 'ercot':
+    elif rto_iso == 'ERCOT':
         df = get_ercot_rt_lmp(years)
     else:
         raise ValueError(f"Unsupported RTO/ISO: {rto_iso}")
